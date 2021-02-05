3e2946c2a27d65875b6bf0cb11ad5dd64ae18bc1
SiYangming <yangming-6863@qq.com>
2021-02-05 22:56:34 +0800

    kidpack_1.32.0

---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+## dummy
+.onLoad <- function(libname, pkgname)
+ message(sprintf("\n*** Deprecation warning ***:\nThe package '%s' is deprecated and will not be supported after Bioconductor release 2.1.\n\n", pkgname))
